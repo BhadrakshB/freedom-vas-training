9d9392ee76f9c254ce37dc4553545b68e76596e9
BhadrakshB <flashbad04@gmail.com>
2025-09-29 16:34:42 +0530

    Changed llm versions

--- src/app/lib/agents/v2/llms.ts
+++ src/app/lib/agents/v2/llms.ts
@@ -3,26 +3,26 @@ import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from "@langchain
 
 
 export const feedbackLLM = new ChatGoogleGenerativeAI({
-    model: process.env.GEMINI_DIALOGUE_MODEL ?? "gemini-1.5-flash",
+    model: process.env.GEMINI_DIALOGUE_MODEL ?? "gemini-2.0-flash",
     temperature: Number(process.env.SUPERVISOR_TEMPERATURE ?? 0.5),
 });
 
 
 export const customerLLM = new ChatGoogleGenerativeAI({
-    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-1.5-flash",
+    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-2.0-flash",
     temperature: Number(process.env.CUSTOMER_TEMPERATURE ?? 0.7),
 });
 
 export const scenarioLLM = new ChatGoogleGenerativeAI({
-    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-1.5-flash",
+    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-2.0-flash",
     temperature: Number(process.env.CUSTOMER_TEMPERATURE ?? 0.8),
 });
 
 export const personaLLM = new ChatGoogleGenerativeAI({
-    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-1.5-flash",
+    model: process.env.GEMINI_DIALOGUE_FAST ?? "gemini-2.0-flash",
     temperature: Number(process.env.CUSTOMER_TEMPERATURE ?? 0.9),
 });
 
 export const embeddings = new GoogleGenerativeAIEmbeddings({
-    model: process.env.GEMINI_EMBEDDING_MODEL ?? "models/embedding-001",    
+    model: process.env.GEMINI_EMBEDDING_MODEL ?? "models/embedding-001",
 });
